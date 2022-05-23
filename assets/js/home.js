e48b5806b8c0148c33fc0fe8c2fa8d9b808739d8
Gengyuan Huang <gengyuan@ualberta.ca>
2022-05-22 23:23:20 -0600

    Add animation to home gallery

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,31 +1,84 @@
+function apply_style(ele, style) {
+    Object.assign(ele.style, style);
+}
+
+const IMAGE_HEIGHT = "450px";
+const IMAGE_WIDTH = "800px";
+
+const gallery_style_init = [
+    {
+        width: `calc(${IMAGE_WIDTH} * 0.8 * 0.75)`,
+        height: `calc(${IMAGE_HEIGHT} * 0.8 * 0.75)`,
+        top: "0",
+        left: "0",
+        zIndex: "997",
+        animationName: "unset",
+        animationDuration: "1s",
+    },
+    {
+        width: `calc(${IMAGE_WIDTH} * 0.8)`,
+        height: `calc(${IMAGE_HEIGHT} * 0.8)`,
+        top: "20px",
+        left: "20%",
+        zIndex: "998",
+        animationName: "unset",
+        animationDuration: "1s",
+    },
+    {
+        width: `${IMAGE_WIDTH}`,
+        height: `${IMAGE_HEIGHT}`,
+        top: "55px",
+        left: "30%",
+        zIndex: "999",
+        animationName: "unset",
+        animationDuration: "1s",
+    }
+]
+
+const gallery_styles_switching = [
+    {
+        width: `calc(${IMAGE_WIDTH} * 0.8 * 0.75)`,
+        height: `calc(${IMAGE_HEIGHT} * 0.8 * 0.75)`,
+        top: "0",
+        left: "0",
+        zIndex: "997",
+        animationName: "fadeOutIn",
+        animationDuration: "1s",
+    },
+    {
+        width: `calc(${IMAGE_WIDTH} * 0.8)`,
+        height: `calc(${IMAGE_HEIGHT} * 0.8)`,
+        top: "20px",
+        left: "20%",
+        zIndex: "998",
+        animationName: "unset",
+        animationDuration: "1s",
+    },
+    {
+        width: `${IMAGE_WIDTH}`,
+        height: `${IMAGE_HEIGHT}`,
+        top: "55px",
+        left: "30%",
+        zIndex: "999",
+        animationName: "unset",
+        animationDuration: "1s",
+    }
+]
 
 window.addEventListener('load', () => {
-    const image_a = document.getElementById('page-home-image-a');
-    const image_b = document.getElementById('page-home-image-b');
-    const image_c = document.getElementById('page-home-image-c');
-
-    image_a.style.backgroundImage = "url(/assets/img/a.png)";
-    image_b.style.backgroundImage = "url(/assets/img/b.png)";
-    image_c.style.backgroundImage = "url(/assets/img/c.png)";
-
-    image_a.classList.add("first");
-    image_b.classList.add("second");
-    image_c.classList.add("third");
+    const images = document.querySelectorAll('#page-home-container .image');
+    let rotate = 0;
     
-    images = [image_a, image_b, image_c];
+    for (let i = 0; i < 3; i++) {
+        apply_style(images[i], gallery_style_init[i]);
+    }
+    rotate = (rotate + 1) % 3;
 
     setInterval(() => {
         for (let i = 0; i < 3; i++) {
-            if (images[i].classList.contains("first")) {
-                images[i].classList.remove("first");
-                images[i].classList.add("second");
-            } else if (images[i].classList.contains("second")){
-                images[i].classList.remove("second");
-                images[i].classList.add("third");
-            } else if (images[i].classList.contains("third")){
-                images[i].classList.remove("third");
-                images[i].classList.add("first");
-            }
+            apply_style(images[i], gallery_styles_switching[(i + rotate) % 3]);
         }
-    }, 3000);
+        rotate = (rotate + 1) % 3;
+        
+    }, 4000);
 });
